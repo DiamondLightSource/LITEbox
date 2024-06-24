4d73e7b20b6d9c5a749ab9d3393045a471e05a64
Giles Knap <giles.knap@diamond.ac.uk>
2024-06-24 14:18:17 +0100

    set up plugins on IOC load

--- kbd101.py
+++ kbd101.py
@@ -1,6 +1,9 @@
 # pip install pyserial
-import serial
+import asyncio
 import time
+
+import aioca
+import serial
 
 com = serial.Serial(
     port="/dev/ttyUSB0",
@@ -13,6 +16,7 @@ com = serial.Serial(
     timeout=1,
 )
 
+
 # https://github.com/Thorlabs/Motion_Control_Examples/blob/main/Python/KCube/KBD101/kbd101_triggering_serial.py
 def send_command(command: str):
     data = bytearray.fromhex(command)
@@ -20,32 +24,41 @@ def send_command(command: str):
     com.flushInput()
     com.flushOutput()
 
+
 def hex4(value: float) -> str:
     # 123456 -> 40E20100
     return int(value).to_bytes(4, "little").hex()
 
 
+async def start_capture():
+    await aioca.caput("BLEC1-EA-DET-01:HDF5:Capture", 1)
+
+
 # https://www.thorlabs.com/Software/Motion%20Control/APT_Communications_Protocol_v38.pdf
 # MGMSG_MOD_SET_CHANENABLESTATE
-send_command('10 02 01 01 50 01')
+send_command("10 02 01 01 50 01")
 
 try:
     # Scale factors from P39 for DDR25 stage
     VSCALE = 26843.5
     ASCALE = 2.74878
-    velocity = 360 # degree/s
-    accl = 1000 # degree/s/s
+    velocity = 360  # degree/s
+    accl = 1000  # degree/s/s
 
     # Calculate velocity and acceleration from
     # MGMSG_MOT_SET_VELPARAMS
-    send_command("13 04 0E 00 D0 01 "
-                "01 00 " +
-                "00 00 00 00 " +
-                hex4(accl * ASCALE) +
-                hex4(velocity * VSCALE))
+    send_command(
+        "13 04 0E 00 D0 01 "
+        "01 00 " + "00 00 00 00 " + hex4(accl * ASCALE) + hex4(velocity * VSCALE)
+    )
 
     # MGMSG_MOT_MOVE_VELOCITY
     send_command("57 04 01 01 50 01")
+
+    time.sleep(1)
+    # start the HDF5 capture
+    asyncio.run(start_capture())
+
     time.sleep(10)
 finally:
     # MGMSG_MOT_MOVE_STOP
