b41c42d2084081746342c9a13071972c604f6e54
Giles Knap <giles.knap@diamond.ac.uk>
2024-06-25 14:09:43 +0100

    bump ioc version

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ pip install -r requirements.txt
 
 # use 'ec' to install the IOC in the local docker instance
 source ./environment.sh
-ec deploy bl01t-di-cam-01 0.1.0
+ec deploy bl01t-di-cam-01 0.1.1
 
 ec ps
 
